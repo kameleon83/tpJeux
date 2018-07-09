feefcb2f89e504822ff2a96102ae246e05156432
Joël Troch <joel.troch62@gmail.com>
2018-07-09 16:36:19 +0200

    Ajout de la classe Main

---
+++ JeuUMLJoel/src/fr/joeltroch/m2iformation/jeu/Main.java
@@ -0,0 +1,15 @@
+package fr.joeltroch.m2iformation.jeu;
+
+/**
+ * Classe principale de l'application.
+ * @author Joël Troch
+ */
+public class Main {
+	/**
+	 * Point d'entrée de l'application.
+	 * @param args Arguments de la ligne de commande.
+	 */
+	public static void main(String[] args) {
+		// TODO
+	}
+}
